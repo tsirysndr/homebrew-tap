3031057a1b796472376509fd1a0c76f907b388db
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-09-05 17:27:58 +0300

    update formula files

--- Formula/datpiff.rb
+++ Formula/datpiff.rb
@@ -2,11 +2,16 @@ class Datpiff < Formula
   desc "A command-line tool for DatPiff Mixtapes"
   homepage "https://github.com/tsirysndr/datpiff"
   url "https://github.com/tsirysndr/datpiff/releases/download/v0.1.11/datpiff_v0.1.11_x86_64-apple-darwin.tar.gz"
-  sha256 "a98ac44d360290d9cfbb9de89f2bf3aa1e037949b6d5ac0a7d154838482d750a"
+  sha256 "36f5cd9ba0ff2ecb1763e3451d2b943d1681f4fa5bb7e72479cec25416ba3946"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/datpiff/releases/download/v0.1.11/datpiff_v0.1.11_x86_64-apple-darwin.tar.gz"
-    sha256 "a98ac44d360290d9cfbb9de89f2bf3aa1e037949b6d5ac0a7d154838482d750a"
+    sha256 "36f5cd9ba0ff2ecb1763e3451d2b943d1681f4fa5bb7e72479cec25416ba3946"
+  end
+
+  if OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/tsirysndr/datpiff/releases/download/v0.1.11/datpiff_v0.1.11_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "372973f4a743f2ebc0457bdc1e88adcb3ebc48307d7361aa48ca4e8cef96706a"
   end
 
   def install
--- Formula/mixtapetorrent.rb
+++ Formula/mixtapetorrent.rb
@@ -1,7 +1,6 @@
 class Mixtapetorrent < Formula
   desc "This is a command-line for mixtapetorrent.com"
   homepage "https://github.com/tsirysndr/mixtapetorrent"
-
   url "https://github.com/tsirysndr/mixtapetorrent/releases/download/v0.1.0/mixtapetorrent_v0.1.0_x86_64-apple-darwin.tar.gz"
   sha256 "4b3c3bb5abb49980ab85b658a6635f4722c283fbdf50034ede8a1b7e3da5bea0"
 
