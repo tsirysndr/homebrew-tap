d2488cdf5bd398d77188b6479960231d61887775
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-09-20 06:25:30 +0000

    update musicplayer formula
    update musicplayer formula

--- Formula/musicplayer.rb
+++ Formula/musicplayer.rb
@@ -1,17 +1,17 @@
 class Musicplayer < Formula
   desc "An extensible music server written in Rust"
   homepage "https://github.com/tsirysndr/music-player"
-  url "https://github.com/tsirysndr/music-player/releases/download/v0.1.3-alpha/music-player_v0.1.3-alpha_x86_64-apple-darwin.tar.gz"
-  sha256 "00383eaf994aac99fd32e0e0f57751e6bf3452abad4bbd439f7b6ab823d49088"
+  url "https://github.com/tsirysndr/music-player/releases/download/v0.1.5-alpha/music-player_v0.1.5-alpha_x86_64-apple-darwin.tar.gz"
+  sha256 "64cbd10f6c8d77a5f073d16a2a789127acc65f11b84808fe46cdbd6717634457"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.3-alpha/music-player_v0.1.3-alpha_x86_64-apple-darwin.tar.gz"
-    sha256 "00383eaf994aac99fd32e0e0f57751e6bf3452abad4bbd439f7b6ab823d49088"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.5-alpha/music-player_v0.1.5-alpha_x86_64-apple-darwin.tar.gz"
+    sha256 "64cbd10f6c8d77a5f073d16a2a789127acc65f11b84808fe46cdbd6717634457"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.3-alpha/music-player_v0.1.3-alpha_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5a506cf48f4eb04bbe994916d359afb41fcda9b19efcc5d14994878e7fd0d120"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.5-alpha/music-player_v0.1.5-alpha_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "10a45be944e49d7aae0af8ad6ec63fd7bd54e68d6b0e7cfaca767b20cee07f34"
   end
 
   def install
