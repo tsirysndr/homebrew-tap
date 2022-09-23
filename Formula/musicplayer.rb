524f20fe8bca07de92e1cab8b8a6f3854fc42f9d
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-09-23 11:43:48 +0000

    update musicplayer binary sha256 hash

--- Formula/musicplayer.rb
+++ Formula/musicplayer.rb
@@ -2,21 +2,21 @@ class Musicplayer < Formula
   desc "An extensible music server written in Rust"
   homepage "https://github.com/tsirysndr/music-player"
   url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-apple-darwin.tar.gz"
-  sha256 "e61cda5d6158b2ea7b686d0e0be527d03eec6edc53230455edee19a5ba5d4640"
+  sha256 "1de7c141d4b1d981ea675e535d74be71d62feba2d65557a6bcec4ccb2677185c"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-apple-darwin.tar.gz"
-    sha256 "e61cda5d6158b2ea7b686d0e0be527d03eec6edc53230455edee19a5ba5d4640"
+    sha256 "1de7c141d4b1d981ea675e535d74be71d62feba2d65557a6bcec4ccb2677185c"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_aarch64-apple-darwin.tar.gz.sha256"
-    sha256 "9c7350dc88055a6fd5f151cec500a3297cccb540fcce870742c09f83a4c5c6a0"
+    sha256 "b3183c16b16a2d30079d2af02f4de5481256345424d7cbaba65f1e1181ea2e84"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "685414ea0b42310cb60472d67831785c6a8fe3664e6815fdceea532480396622"
+    sha256 "2cd0204287850d8d6845c14773ce776fdbce5542ae4e980583116ada1831944a"
   end
 
   def install
