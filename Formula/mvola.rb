f1eeb992d5670a60c9a2585ab9ac35802721ee4d
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-09-24 18:52:06 +0300

    update mvola formula

--- Formula/mvola.rb
+++ Formula/mvola.rb
@@ -2,16 +2,21 @@ class Mvola < Formula
   desc "Command-line tool for MVola API"
   homepage "https://github.com/tsirysndr/mvola-cli"
   url "https://github.com/tsirysndr/mvola-cli/releases/download/0.1.4/mvola_0.1.4_x86_64-apple-darwin.tar.gz"
-  sha256 "b28c4b2aba91c1eb5b97c0bdc40795a3ec659932e86bae04364841fe855b4d58"
+  sha256 "4c84b02c3230f95c01e35448b47e52223b49742251c21ffe613b35564cbb056a"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/mvola-cli/releases/download/0.1.4/mvola_0.1.4_x86_64-apple-darwin.tar.gz"
-    sha256 "b28c4b2aba91c1eb5b97c0bdc40795a3ec659932e86bae04364841fe855b4d58"
+    sha256 "4c84b02c3230f95c01e35448b47e52223b49742251c21ffe613b35564cbb056a"
+  end
+
+  if OS.mac? && Hardware::CPU.arm?
+    url "https://github.com/tsirysndr/mvola-cli/releases/download/0.1.4/mvola_0.1.4_aarch64-apple-darwin.tar.gz"
+    sha256 "f15eb4e1ab8bb3f7dac11d2f4e3811c19c5778a45f84d9105fa54239a746e3e7"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/mvola-cli/releases/download/0.1.4/mvola_0.1.4_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "270d61d8480163b71981faf866ee3dc3f457595ef841c975635f7274a6bf7f7a"
+    sha256 "5c329fa8b2af3e618f98cc02faa48823e14d71305b5291fb7043a30e08bcd9da"
   end
 
   def install
