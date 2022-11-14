e7c714dd7fbd73b8962ee2a849a7d9e51c952224
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-11-14 09:51:45 +0300

    update musicplayer formula

--- Formula/musicplayer.rb
+++ Formula/musicplayer.rb
@@ -1,22 +1,22 @@
 class Musicplayer < Formula
   desc "An extensible music server written in Rust"
   homepage "https://github.com/tsirysndr/music-player"
-  url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-apple-darwin.tar.gz"
-  sha256 "1de7c141d4b1d981ea675e535d74be71d62feba2d65557a6bcec4ccb2677185c"
+  url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.0/music-player_v0.2.0-alpha.0_x86_64-apple-darwin.tar.gz"
+  sha256 "d28010ac2b982a58ee034a008f4d7f947745041cad382b3e27dc23a1586eba40"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-apple-darwin.tar.gz"
-    sha256 "1de7c141d4b1d981ea675e535d74be71d62feba2d65557a6bcec4ccb2677185c"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.0/music-player_v0.2.0-alpha.0_x86_64-apple-darwin.tar.gz"
+    sha256 "d28010ac2b982a58ee034a008f4d7f947745041cad382b3e27dc23a1586eba40"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_aarch64-apple-darwin.tar.gz"
-    sha256 "b3183c16b16a2d30079d2af02f4de5481256345424d7cbaba65f1e1181ea2e84"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.0/music-player_v0.2.0-alpha.0_aarch64-apple-darwin.tar.gz"
+    sha256 "c4d483e5677e74a5a8223dfb0a9c03be84baf1dd8d6a697457354148349126ce"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.1.6-alpha/music-player_v0.1.6-alpha_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "2cd0204287850d8d6845c14773ce776fdbce5542ae4e980583116ada1831944a"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0-alpha.0/music-player_v0.2.0-alpha.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "a4d26b3d0ee82c5c4c9d51c65a746cd50579eded34aa450459809b7f6424caf"
   end
 
   def install
