ee27f554762a45ec5dc9e9f59c410851f1563102
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2022-12-06 09:57:41 +0300

    fix musicplayer cask

--- Casks/musicplayer.rb
+++ Casks/musicplayer.rb
@@ -2,7 +2,7 @@ cask "musicplayer" do
   version "0.2.0-alpha.8"
   sha256 "f37ad80174e28fba6aa27b427409d26cfa8c8c72898ab5412bbaa5b21660b83c"
 
-  url "https://github.com/tsirysndr/music-player/releases/download/#{version}/Music_Player_#{version}_x64.dmg"
+  url "https://github.com/tsirysndr/music-player/releases/download/v#{version}/Music_Player_v#{version}_x64.dmg"
   name "musicplayer"
   desc "Music Player Desktop App"
   homepage "https://github.com/tsirysndr/music-player"
