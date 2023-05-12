f565d55509126e23715bc3cb5bac43b7c4e243eb
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-05-13 00:58:15 +0300

    update superviseur formula

--- Formula/superviseur.rb
+++ Formula/superviseur.rb
@@ -1,22 +1,22 @@
 class Superviseur < Formula
-  desc "A simple process supervisor for UNIX-like systems."
+  desc "Define and run multi-service applications on isolated environments with Nix or Docker"
   homepage "https://github.com/tsirysndr/superviseur"
-  url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.5/superviseur_v0.1.0-alpha.5_x86_64-apple-darwin.tar.gz"
-  sha256 "6f8575185e539473a046a398f92e820dfafce6f03c7cb148c68ad7229027e31e"
+  url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.6/superviseur_v0.1.0-alpha.6_x86_64-apple-darwin.tar.gz"
+  sha256 "8876165e1486ba174bd54eb1e0f7f0a509188daeb8be47865f6668bf422c357a"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.5/superviseur_v0.1.0-alpha.5_x86_64-apple-darwin.tar.gz"
-    sha256 "6f8575185e539473a046a398f92e820dfafce6f03c7cb148c68ad7229027e31e"
+    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.6/superviseur_v0.1.0-alpha.6_x86_64-apple-darwin.tar.gz"
+    sha256 "8876165e1486ba174bd54eb1e0f7f0a509188daeb8be47865f6668bf422c357a"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.5/superviseur_v0.1.0-alpha.5_aarch64-apple-darwin.tar.gz"
-    sha256 "6a558a5723a1e0ca46f6f7c4461244b405fc1056ab1ba1e61ebc332b2d480544"
+    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.6/superviseur_v0.1.0-alpha.6_aarch64-apple-darwin.tar.gz"
+    sha256 "d39b22d497c309b14dd219810b87ed145ab01e0503ee665b94c7066b58fd0cab"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.5/superviseur_v0.1.0-alpha.5_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "757f8a9f89a5d327cd02b258b10c3411dfee8da748fb71d248e040bdffad94c0"
+    url "https://github.com/tsirysndr/superviseur/releases/download/v0.1.0-alpha.6/superviseur_v0.1.0-alpha.6_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "b39b1f4660188b569180348e3ba3474944504d2aa6ebe485b244c817c4044853"
   end
 
   def install
