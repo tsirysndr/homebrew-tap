33191ebdca23c5d68ff43a295d349d9e5439c1a7
Tsiry Sandratraina <tsiry.sndr@gmail.com>
2023-08-03 02:43:27 +0000

    update piratebay Formula

--- Formula/piratebay.rb
+++ Formula/piratebay.rb
@@ -1,12 +1,12 @@
 class Piratebay < Formula
   desc "A command-line tool written in Rust to search for torrents on piratebay"
   homepage "https://github.com/tsirysndr/piratebay"
-  url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.5/piratebay_v0.1.5_x86_64-apple-darwin.tar.gz"
-  sha256 "717a0753d1bc04d7bcee29c2c8b7cd0c0760a477a0901190ce532dfbc869af82"
+  url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.6/piratebay_v0.1.6_x86_64-apple-darwin.tar.gz"
+  sha256 "b55b2b154560e17636379b97608710d54f7e29efe734ec5447a466f646d65235"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.5/piratebay_v0.1.5_x86_64-apple-darwin.tar.gz"
-    sha256 "717a0753d1bc04d7bcee29c2c8b7cd0c0760a477a0901190ce532dfbc869af82"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.6/piratebay_v0.1.6_x86_64-apple-darwin.tar.gz"
+    sha256 "b55b2b154560e17636379b97608710d54f7e29efe734ec5447a466f646d65235"
   end
 
   def install
