69604350038e32d8f1633dfffa959b22f025f9a7
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2023-10-11 08:10:44 +0000

    update `envhub` Formula

--- Formula/envhub.rb
+++ Formula/envhub.rb
@@ -1,22 +1,22 @@
 class Envhub < Formula
   desc "Manage your dotfiles and packages with ease. Define your $HOME as Code 💻 🚀 ✨"
   homepage "https://github.com/tsirysndr/envhub"
-  url "https://github.com/tsirysndr/envhub/releases/download/v0.2.0/envhub_v0.2.0_x86_64-apple-darwin.tar.gz"
-  sha256 "2f304477f71031ea476097f4ed2b19281904ece105ef25ad0a68c7c46ae5521e"
+  url "https://github.com/tsirysndr/envhub/releases/download/v0.2.1/envhub_v0.2.1_x86_64-apple-darwin.tar.gz"
+  sha256 "b8f18dc4a3af196fe8d4dcba5622d74bbe7f55b07c6710f87ebe92349bc93707"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.0/envhub_v0.2.0_x86_64-apple-darwin.tar.gz"
-    sha256 "2f304477f71031ea476097f4ed2b19281904ece105ef25ad0a68c7c46ae5521e"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.1/envhub_v0.2.1_x86_64-apple-darwin.tar.gz"
+    sha256 "b8f18dc4a3af196fe8d4dcba5622d74bbe7f55b07c6710f87ebe92349bc93707"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.0/envhub_v0.2.0_aarch64-apple-darwin.tar.gz"
-    sha256 "cccede4b37bbb4f523487b0c4d5d0ed5cf0c3baf08c56385ed0ccd7e519db6db"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.1/envhub_v0.2.0_aarch64-apple-darwin.tar.gz"
+    sha256 "fc12b7bbb07589e8fc51891eaf03a4e926b79996fa30ddebc589cc860c4501d2"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.0/envhub_v0.2.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "bf7546ca22cd00f871abd8f663654e372b4bb6f2d1e8a0b23da1f49af2b47d20"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.0/envhub_v0.2.1_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "9bd5df60efe9830c87e0131ffcc52664838779648b913971197ff63f984791f8"
   end
 
   def install
