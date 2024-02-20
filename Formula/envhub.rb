9832d5338ac487ec6d06310eda1103db2982e689
Tsiry Sandratraina <tsiry.sndr@fluentci.io>
2024-02-20 21:25:00 +0000

    update envhub Formula

--- Formula/envhub.rb
+++ Formula/envhub.rb
@@ -1,22 +1,22 @@
 class Envhub < Formula
   desc "Manage your dotfiles and packages with ease. Define your $HOME as Code 💻 🚀 ✨"
   homepage "https://github.com/tsirysndr/envhub"
-  url "https://github.com/tsirysndr/envhub/releases/download/v0.2.10/envhub_v0.2.10_x86_64-apple-darwin.tar.gz"
-  sha256 "3c0d652814a9fa597025279469beb117c67aa12c6cdad0ca446269bbb896af8f"
+  url "https://github.com/tsirysndr/envhub/releases/download/v0.2.11/envhub_v0.2.11_x86_64-apple-darwin.tar.gz"
+  sha256 "eb82da02e69c46c239386b998030db9d9d344b593a02e8cc39471a4e4e20e1fe"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.10/envhub_v0.2.10_x86_64-apple-darwin.tar.gz"
-    sha256 "3c0d652814a9fa597025279469beb117c67aa12c6cdad0ca446269bbb896af8f"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.11/envhub_v0.2.11_x86_64-apple-darwin.tar.gz"
+    sha256 "eb82da02e69c46c239386b998030db9d9d344b593a02e8cc39471a4e4e20e1fe"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.10/envhub_v0.2.10_aarch64-apple-darwin.tar.gz"
-    sha256 "386ad6015501bdc40678ba48ca1764d89e687dec497e4363b9dfb100dd841f64"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.11/envhub_v0.2.11_aarch64-apple-darwin.tar.gz"
+    sha256 "131c16c4006ef6675ff189939a3220fe1b79d1ffa57f034f1568db6678748143"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.10/envhub_v0.2.10_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4b8cde8510c4658af05e38a4823294c4aec6de09cc89a4a225de92e87e9a09d0"
+    url "https://github.com/tsirysndr/envhub/releases/download/v0.2.11/envhub_v0.2.11_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "e758033a431a9db483c5abb85785859980f8a5604a200050691c993d442d0a97"
   end
 
   def install
