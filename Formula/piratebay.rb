971a9aa88d92ee61e28b24a9e789c0316df54dd3
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2025-12-17 11:53:20 +0300

    update piratebay Formula

--- Formula/piratebay.rb
+++ Formula/piratebay.rb
@@ -1,22 +1,22 @@
 class Piratebay < Formula
   desc "A command-line tool written in Rust to search for torrents on piratebay"
   homepage "https://github.com/tsirysndr/piratebay"
-  url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.9/piratebay_v0.1.9_x86_64-apple-darwin.tar.gz"
-  sha256 ""
+  url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.0/piratebay_v0.2.0_x86_64-apple-darwin.tar.gz"
+  sha256 "12542e507a632a20d1f5a2dff4d7926b3bec6c2f490475a036cf4421b07df740"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.9/piratebay_v0.1.9_x86_64-apple-darwin.tar.gz"
-    sha256 "c9576aafd84b07b2daf4a137c0f2e14954f801e4723b6f314a28e04ac6a6566d"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.0/piratebay_v0.2.0_x86_64-apple-darwin.tar.gz"
+    sha256 "12542e507a632a20d1f5a2dff4d7926b3bec6c2f490475a036cf4421b07df740"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.9/piratebay_v0.1.9_aarch64-apple-darwin.tar.gz"
-    sha256 "6ff2b751de18ab3256b41734d9251a3acce05da4f18cd5ad35127bf49b6c7c71"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.0/piratebay_v0.2.0_aarch64-apple-darwin.tar.gz"
+    sha256 "af5c15acd7f9104cd41a3c5d9f71446a923d214d518f68ef21d2e37efe67b9d6"
   end
   
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.1.9/piratebay_v0.1.9_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "d9eff29fc3b20a2a82f7a2ae93be3be8f623dd14d86ec836dc8df4dd39cfc2e0"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.0/piratebay_v0.2.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "79ee6b4944e2d66780e6dec803b77a2a8a7875f5fa91b20fc45cea7897bc0796"
   end
 
   def install
