79ead81d0796e25e69ddd64c20a3a9b9573d30f2
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-08 21:13:23 +0300

    Update SHA256 checksums for Rockbox formula

--- Formula/rockbox.rb
+++ Formula/rockbox.rb
@@ -2,11 +2,11 @@ class Rockbox < Formula
   desc "A modern Music Player Daemon based on Rockbox firmware"
   homepage "https://github.com/tsirysndr/rockbox-zig"
   url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.08/rockbox_2026.06.08_aarch64-darwin.tar.gz"
-  sha256 ""
+  sha256 "b92c06fe5a354f3a5d8e46732cca03609a3be8b36bc97d1f29ecb33f43fc5f49"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.08/rockbox_2026.06.08_x86_64-darwin.tar.gz"
-    sha256 "b92c06fe5a354f3a5d8e46732cca03609a3be8b36bc97d1f29ecb33f43fc5f49"
+    sha256 "ad70ad0f266d238e65fd6e0d269e4f1b396503272c55db6d2be0cf79c9d2c1ca"
   end
 
   if OS.mac? && Hardware::CPU.arm?
