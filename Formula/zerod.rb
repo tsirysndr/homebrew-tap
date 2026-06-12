bfa8f3319ba9c99403e3f92ad54b438a7bf5c969
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-12 16:39:44 +0300

    Update zerod to version 0.3.1

--- Formula/zerod.rb
+++ Formula/zerod.rb
@@ -1,22 +1,22 @@
 class Zerod < Formula
   desc "Headless gRPC daemon for Linux audio: Bluetooth, HLS/DASH and systemd"
   homepage "https://github.com/tsirysndr/zerod"
-  url "https://github.com/tsirysndr/zerod/releases/download/v0.3.0/zerod-aarch64-apple-darwin.tar.gz"
-  sha256 "10d712d99899742a9d24c902ddee941044d9c06e86ba9f4fdd571e5a340af68b"
+  url "https://github.com/tsirysndr/zerod/releases/download/v0.3.1/zerod-aarch64-apple-darwin.tar.gz"
+  sha256 "45a0a9d0e4103616ee909b030d4d0194c28dfa81e34d8aa06fc8f12064179e0b"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.0/zerod-x86_64-apple-darwin.tar.gz"
-    sha256 "16e5cc8ee586bba5874ba7ddbe05e74af7b499622edc71dd4a42c619320ce1ff"
+    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.1/zerod-x86_64-apple-darwin.tar.gz"
+    sha256 "7043f7459f0b1f6a322f1d6be960dddb82e54923d5f5f959e52e25d8d1ab3caf"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.0/zerod-aarch64-apple-darwin.tar.gz"
-    sha256 "10d712d99899742a9d24c902ddee941044d9c06e86ba9f4fdd571e5a340af68b"
+    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.1/zerod-aarch64-apple-darwin.tar.gz"
+    sha256 "45a0a9d0e4103616ee909b030d4d0194c28dfa81e34d8aa06fc8f12064179e0b"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.0/zerod-x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "86fb69c857c7edd82126c2f652bfd9e6caa4067bab48193db4c1fed07b7ddb08"
+    url "https://github.com/tsirysndr/zerod/releases/download/v0.3.1/zerod-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "69e2d554fb4a7355ab2f9bbf0dd94bb5278e87bc2892f4dc8f2a9da8c3bd72d0"
   end
 
   def install
