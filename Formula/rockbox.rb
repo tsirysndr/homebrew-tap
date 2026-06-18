8a222eca2b2c89c6474e05d1960bb30b730faed6
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-18 19:54:12 +0300

    Update rockbox.rb

--- Formula/rockbox.rb
+++ Formula/rockbox.rb
@@ -1,22 +1,22 @@
 class Rockbox < Formula
   desc "A modern Music Player Daemon based on Rockbox firmware"
   homepage "https://github.com/tsirysndr/rockbox-zig"
-  url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.14/rockbox_2026.06.14_aarch64-darwin.tar.gz"
-  sha256 "a3a28697422da47518814c4d7c87cb2516c1ab918e0a8d6d4a863824985f7f47"
+  url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.18/rockbox_2026.06.18_aarch64-darwin.tar.gz"
+  sha256 "4b4771cc80d916ea1b20aaa10f1077a2d5e4c350036f2f8205d9f2b4f05cb2d1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.14/rockbox_2026.06.14_x86_64-darwin.tar.gz"
-    sha256 "e843c2b099b8800ff6c8b7c5083d52afed73741b6c65445556c16afab43ad867"
+    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.18/rockbox_2026.06.18_x86_64-darwin.tar.gz"
+    sha256 "283b9865c9c6d9e509102ee7edc49ee3be783734803d108c516c27f24e632b77"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.14/rockbox_2026.06.14_aarch64-darwin.tar.gz"
-    sha256 "a3a28697422da47518814c4d7c87cb2516c1ab918e0a8d6d4a863824985f7f47"
+    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.18/rockbox_2026.06.18_aarch64-darwin.tar.gz"
+    sha256 "4b4771cc80d916ea1b20aaa10f1077a2d5e4c350036f2f8205d9f2b4f05cb2d1"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.14/rockbox_2026.06.14_x86_64-linux.tar.gz"
-    sha256 "d71146e6f9250538d029d2a71a7ba560ac23d48aceef0817dfb7831d715ef748"
+    url "https://github.com/tsirysndr/rockbox-zig/releases/download/2026.06.18/rockbox_2026.06.18_x86_64-linux.tar.gz"
+    sha256 "788f559db25a33e34b425fbf66531b50f1b04e04787a50909bac61bb421499e8"
   end
 
   def install
