def0ad0642c2defa7e533cb66213dc632dd5eb2e
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-24 15:30:28 +0300

    Update piratebay.rb to version 0.3.0

--- Formula/piratebay.rb
+++ Formula/piratebay.rb
@@ -1,22 +1,22 @@
 class Piratebay < Formula
   desc "A command-line tool written in Rust to search for torrents on piratebay"
   homepage "https://github.com/tsirysndr/piratebay"
-  url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.1/piratebay_v0.2.1_x86_64-apple-darwin.tar.gz"
-  sha256 "2f1584a8ed52a84b9c400f77362266ff35cbb9fbea81356d96f488ee0d5103af"
+  url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-apple-darwin.tar.gz"
+  sha256 "9d8b9a61c71098169d6532591a7c67652903e930cca7817a107dac0ec592ec74"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.1/piratebay_v0.2.1_x86_64-apple-darwin.tar.gz"
-    sha256 "2f1584a8ed52a84b9c400f77362266ff35cbb9fbea81356d96f488ee0d5103af"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-apple-darwin.tar.gz"
+    sha256 "9d8b9a61c71098169d6532591a7c67652903e930cca7817a107dac0ec592ec74"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.1/piratebay_v0.2.1_aarch64-apple-darwin.tar.gz"
-    sha256 "20c8d5803716e36754319547ea6d54b24f11a34eb71693b71f1d429eb429594f"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_aarch64-apple-darwin.tar.gz"
+    sha256 "cd20c2d8af7c453831b94e42a0fbcdf83d900c1da32a2713da1fd71a45bc3c25"
   end
   
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.2.1/piratebay_v0.2.1_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "d06bf3d8e43a570ee7a1a2ea48227b6deda7ae244628954a77ffb8a98ed6aa49"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "12330a444bce1b4d23fded131de4939ac446887f5b72d548f93859fc2e1834df"
   end
 
   def install
