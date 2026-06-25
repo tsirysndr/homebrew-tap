ba84bc5ca31cb7519894def2c1436952f6d45a99
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-25 13:39:27 +0300

    Update Smolsonic to version 0.4.0

--- Formula/smolsonic.rb
+++ Formula/smolsonic.rb
@@ -1,22 +1,22 @@
 class Smolsonic < Formula
   desc "A tiny Subsonic server in Rust"
   homepage "https://github.com/tsirysndr/smolsonic"
-  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.3.0/smolsonic-v0.3.0-macos-aarch64.tar.gz"
-  sha256 "f4cf9f71dee465d72a2fdafab9c3a820e00324adcf9a2685eb2e584ca423d4b3"
+  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.4.0/smolsonic-v0.4.0-macos-aarch64.tar.gz"
+  sha256 "4ac82bcd1ac1200e35fa3bf9e68027451b4f87419d6a5cdfc5357c60305e418c"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.3.0/smolsonic-v0.3.0-macos-amd64.tar.gz"
-    sha256 "d0e0c46539a0adb80ac77495ca7e8cdb3e4f7603cf34f9b32e452d3600aeeb53"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.4.0/smolsonic-v0.4.0-macos-amd64.tar.gz"
+    sha256 "a3a2a47639811ce59e91bea0e4e2238c07a3acff9909b8358887f7987355f78f"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.3.0/smolsonic-v0.3.0-macos-aarch64.tar.gz"
-    sha256 "f4cf9f71dee465d72a2fdafab9c3a820e00324adcf9a2685eb2e584ca423d4b3"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.4.0/smolsonic-v0.4.0-macos-aarch64.tar.gz"
+    sha256 "4ac82bcd1ac1200e35fa3bf9e68027451b4f87419d6a5cdfc5357c60305e418c"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.3.0/smolsonic-v0.3.0-linux-amd64.tar.gz"
-    sha256 "f76e78871d5ad75782ac41d8374d1590b3d411b7becf663e27b429af70a7ef85"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.4.0/smolsonic-v0.4.0-linux-amd64.tar.gz"
+    sha256 "4ca74eaca8c63b62dcf206ce4bd9ec2c9dbc148a08c21dfc7236db0bad00a38c"
   end
 
   def install
