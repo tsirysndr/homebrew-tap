760bbb1fbd0a9fe2db09c318f1eb98cf38304596
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-06-29 17:37:10 +0300

    Update Smolsonic to version 0.6.0

--- Formula/smolsonic.rb
+++ Formula/smolsonic.rb
@@ -1,23 +1,23 @@
 class Smolsonic < Formula
   desc "A tiny Subsonic server in Rust"
   homepage "https://github.com/tsirysndr/smolsonic"
-  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.5.3/smolsonic-v0.5.3-macos-aarch64.tar.gz"
-  sha256 "ba0e22bc81de693ff22c92932e32c151c76364977757a51a278257c03c215deb"
-  version "0.5.3"
+  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.6.0/smolsonic-v0.6.0-macos-aarch64.tar.gz"
+  sha256 "46f225073e0fa78cbb0324e4d4f1aebdcd1a098a0a73a57793baffb0bdc2e9e7"
+  version "0.6.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.5.3/smolsonic-v0.5.3-macos-amd64.tar.gz"
-    sha256 "83a58cfe47d30edeaadeb387d25ff49f102a6573b0f1f30ad2fe8be4e3f3707a"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.6.0/smolsonic-v0.6.0-macos-amd64.tar.gz"
+    sha256 "41873cf0a77136a16bb37b28c1a435c0f1d4f187feeb5ce451549d8af6ecd9b2"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.5.3/smolsonic-v0.5.3-macos-aarch64.tar.gz"
-    sha256 "ba0e22bc81de693ff22c92932e32c151c76364977757a51a278257c03c215deb"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.6.0/smolsonic-v0.6.0-macos-aarch64.tar.gz"
+    sha256 "46f225073e0fa78cbb0324e4d4f1aebdcd1a098a0a73a57793baffb0bdc2e9e7"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.5.3/smolsonic-v0.5.3-linux-amd64.tar.gz"
-    sha256 "228e9cc1cf4fe426ad30f61a79c32e85f87bbf756bd17be1e81f05279e52a160"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.6.0/smolsonic-v0.6.0-linux-amd64.tar.gz"
+    sha256 "b580f3056acc21f1c21eb0d4e2fd3d553f3672c05e6f33e34bcf385324b91351"
   end
 
   def install
