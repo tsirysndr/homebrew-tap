c4e1e1b9f396ca73d591941f047af46b0abe5666
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-03 11:18:59 +0300

    Update fin.rb for version 0.2.0 release

--- Formula/fin.rb
+++ Formula/fin.rb
@@ -1,25 +1,25 @@
 class Fin < Formula
-  desc "a neon-electric TUI Jellyfin client for mpv & Chromecast"
+  desc "a neon-electric TUI Jellyfin client for mpv, Chromecast & UPnP MediaRenderer"
   homepage "https://github.com/tsirysndr/fin"
-  url "https://github.com/tsirysndr/fin/releases/download/v0.1.0/fin-v0.1.0-macos-aarch64.tar.gz"
-  sha256 "c60a4739962c51855dcfee384602b421cbf608210e2a206a8dd2eb4878a12f34"
-  version "0.1.0"
+  url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-aarch64.tar.gz"
+  sha256 "b6e6f709aa2e5990956c76a673691929c53635f17711d30eff16bb2137768d08"
+  version "0.2.0"
 
   depends_on "mpv"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.1.0/fin-v0.1.0-macos-amd64.tar.gz"
-    sha256 "d01baa3952ae4c14b028890f894af9f69b9fdfe78d2183ac7db266e51f15140b"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-amd64.tar.gz"
+    sha256 "dab8deb17fb4ab61a5d1aabbcb33ddd3b8673ed5d9e83d86dbbc286a0a69b8fd"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.1.0/fin-v0.1.0-macos-aarch64.tar.gz"
-    sha256 "c60a4739962c51855dcfee384602b421cbf608210e2a206a8dd2eb4878a12f34"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-aarch64.tar.gz"
+    sha256 "b6e6f709aa2e5990956c76a673691929c53635f17711d30eff16bb2137768d08"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.1.0/fin-v0.1.0-linux-amd64.tar.gz"
-    sha256 "901296c09067e8ebaff3fa17f894269cf8d3e880e6a4aacd899caa596989bf0e"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-linux-amd64.tar.gz"
+    sha256 "c8f12b25c3dc918b4c17c38f29d4cb4ae05d6556887c7bed4085f80f49ad163b"
   end
 
   def install
