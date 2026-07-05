9e2b6f333e16b57bd5a8a2e30ee2d26ccfb48e69
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-06 01:55:23 +0300

    Update Tunein CLI to version 0.5.0

--- Formula/tunein.rb
+++ Formula/tunein.rb
@@ -1,22 +1,22 @@
 class Tunein < Formula
   desc "Browse and listen to thousands of radio stations across the globe right from your terminal"
   homepage "https://github.com/tsirysndr/tunein-cli"
-  url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.4.2/tunein_v0.4.2_aarch64-apple-darwin.tar.gz"
-  sha256 "0f52ff319ec151da0faf7fd6fd45a9b17c4f5f59186f1aa664f8bfc938b9d26a"
+  url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.5.0/tunein_v0.5.0_aarch64-apple-darwin.tar.gz"
+  sha256 "af838e8f2458899746a36e60c1db322d7f8830b521a7b790f0c6ad24a7651cea"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.4.2/tunein_v0.4.2_x86_64-apple-darwin.tar.gz"
-    sha256 "24896b2e66c32dc963af86fd6999612a36fb136e0e0ee5a02860d9ac36cd54ad"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.5.0/tunein_v0.5.0_x86_64-apple-darwin.tar.gz"
+    sha256 "f9a82d145fe3aae9998b953faecc352a096f40c2141c88e300404b196535058b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.4.2/tunein_v0.4.2_aarch64-apple-darwin.tar.gz"
-    sha256 "0f52ff319ec151da0faf7fd6fd45a9b17c4f5f59186f1aa664f8bfc938b9d26a"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.5.0/tunein_v0.5.0_aarch64-apple-darwin.tar.gz"
+    sha256 "af838e8f2458899746a36e60c1db322d7f8830b521a7b790f0c6ad24a7651cea"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.4.2/tunein_v0.4.2_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "0975df3e213d4a23714036a1c349a7bf29df8feb84f5cc729928c92301e76a57"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.5.0/tunein_v0.5.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "e2d4c374432a7708ebe12e391a373e5a5f24f3b9c972206c6accaf1d6d9638a3"
   end
 
   def install
