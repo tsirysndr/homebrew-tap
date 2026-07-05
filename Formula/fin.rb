1bea8b851200471e741fbe98ebb74eca4a02f46c
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-05 22:51:13 +0300

    Update fin.rb to version 0.3.1

--- Formula/fin.rb
+++ Formula/fin.rb
@@ -1,25 +1,25 @@
 class Fin < Formula
   desc "a neon-electric TUI Jellyfin & Navidrome client - powered by symphonia, mpv, Chromecast & UPnP MediaRenderer"
   homepage "https://github.com/tsirysndr/fin"
-  url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-aarch64.tar.gz"
-  sha256 "1e18f182ce2d18bb45ee9cec1e28975bceb294e7c4efe4afa0f53f8ea3fe887a"
-  version "0.3.0"
+  url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-aarch64.tar.gz"
+  sha256 "7b755b4790cf4719f06b4bfe461f0c7f017e5464b94bf931b429fdcae27f3e86"
+  version "0.3.1"
 
   depends_on "mpv"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-amd64.tar.gz"
-    sha256 "a0612a1a1a02c7e2c72d0af82abbdbbd5729074a298933ab29a1f350569345ea"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-amd64.tar.gz"
+    sha256 "fa78474bddb92fa2e40e8e10ae25dfe38dc9b1a48abbc08ce1fad546c474e1dc"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-aarch64.tar.gz"
-    sha256 "1e18f182ce2d18bb45ee9cec1e28975bceb294e7c4efe4afa0f53f8ea3fe887a"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-aarch64.tar.gz"
+    sha256 "7b755b4790cf4719f06b4bfe461f0c7f017e5464b94bf931b429fdcae27f3e86"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-linux-amd64.tar.gz"
-    sha256 "90e76e6a9528b1d2e5ccbcf965f5d87730ac70baa26bb5add20013edcebaa61d"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-linux-amd64.tar.gz"
+    sha256 "0bf4f03d642932308193cff97036b2fa01006fc92d44d3d1e65f04c7048d848d"
   end
 
   def install
