db2f0608630581eaf56cc257672bde6361430c52
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-05 20:48:38 +0300

    Update Fin to version 0.3.0 with new dependencies

--- Formula/fin.rb
+++ Formula/fin.rb
@@ -1,25 +1,25 @@
 class Fin < Formula
-  desc "a neon-electric TUI Jellyfin client for mpv, Chromecast & UPnP MediaRenderer"
+  desc "a neon-electric TUI Jellyfin & Navidrome client - powered by symphonia, mpv, Chromecast & UPnP MediaRenderer"
   homepage "https://github.com/tsirysndr/fin"
-  url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-aarch64.tar.gz"
-  sha256 "b6e6f709aa2e5990956c76a673691929c53635f17711d30eff16bb2137768d08"
-  version "0.2.0"
+  url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-aarch64.tar.gz"
+  sha256 "1e18f182ce2d18bb45ee9cec1e28975bceb294e7c4efe4afa0f53f8ea3fe887a"
+  version "0.3.0"
 
   depends_on "mpv"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-amd64.tar.gz"
-    sha256 "dab8deb17fb4ab61a5d1aabbcb33ddd3b8673ed5d9e83d86dbbc286a0a69b8fd"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-amd64.tar.gz"
+    sha256 "a0612a1a1a02c7e2c72d0af82abbdbbd5729074a298933ab29a1f350569345ea"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-macos-aarch64.tar.gz"
-    sha256 "b6e6f709aa2e5990956c76a673691929c53635f17711d30eff16bb2137768d08"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-macos-aarch64.tar.gz"
+    sha256 "1e18f182ce2d18bb45ee9cec1e28975bceb294e7c4efe4afa0f53f8ea3fe887a"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.2.0/fin-v0.2.0-linux-amd64.tar.gz"
-    sha256 "c8f12b25c3dc918b4c17c38f29d4cb4ae05d6556887c7bed4085f80f49ad163b"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.3.0/fin-v0.3.0-linux-amd64.tar.gz"
+    sha256 "90e76e6a9528b1d2e5ccbcf965f5d87730ac70baa26bb5add20013edcebaa61d"
   end
 
   def install
