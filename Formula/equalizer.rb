a3b7a90739531460ded7f5d3c1a9419a210b966d
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-06 23:15:47 +0300

    Update equalizer to version 0.2.0

--- Formula/equalizer.rb
+++ Formula/equalizer.rb
@@ -1,22 +1,22 @@
 class Equalizer < Formula
   desc "Pipe raw PCM through the Rockbox 10-band equalizer to the sound card, with a ratatui front-end"
   homepage "https://github.com/tsirysndr/equalizer"
-  url "https://github.com/tsirysndr/equalizer/releases/download/v0.1.0/equalizer-v0.1.0-macos-aarch64.tar.gz"
-  sha256 "3cb21c93648deca00a053a2ebcd320831b71add570e75ffdde9eaebaba7cefb2"
+  url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-macos-aarch64.tar.gz"
+  sha256 "3495dae7ee9b3bf39acd210f27367933468df1a5c3536b4157c97dd7e59193c3"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/equalizer/releases/download/v0.1.0/equalizer-v0.1.0-macos-amd64.tar.gz"
-    sha256 "c5ddd48cb0aa61a658d7355408063fe43b03db46cc75a88bd19bc9ec5c41545e"
+    url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-macos-amd64.tar.gz"
+    sha256 "9f6b7371fe0f1e9d313e02ec1d34d15aa05b20caa34b6abf077655811a4c78ea"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/equalizer/releases/download/v0.1.0/equalizer-v0.1.0-macos-aarch64.tar.gz"
-    sha256 "3cb21c93648deca00a053a2ebcd320831b71add570e75ffdde9eaebaba7cefb2"
+    url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-macos-aarch64.tar.gz"
+    sha256 "3495dae7ee9b3bf39acd210f27367933468df1a5c3536b4157c97dd7e59193c3"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/equalizer/releases/download/v0.1.0/equalizer-v0.1.0-linux-amd64.tar.gz"
-    sha256 "788f0233ad36dccbcd69be0869097ae22afe9fa756789b23163b02e29a46f9e5"
+    url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-linux-amd64.tar.gz"
+    sha256 "22976cd2bdf615442eb66bf4f5888e08b2078e8fe635ab54470c527703426dad"
   end
 
   def install
