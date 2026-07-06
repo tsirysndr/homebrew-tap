b0248fa7997f9e90af086c8c64dfbba1007dae6f
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-06 23:17:18 +0300

    Update equalizer.rb

--- Formula/equalizer.rb
+++ Formula/equalizer.rb
@@ -3,7 +3,8 @@ class Equalizer < Formula
   homepage "https://github.com/tsirysndr/equalizer"
   url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-macos-aarch64.tar.gz"
   sha256 "3495dae7ee9b3bf39acd210f27367933468df1a5c3536b4157c97dd7e59193c3"
-
+  version "0.2.0"
+  
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/equalizer/releases/download/v0.2.0/equalizer-v0.2.0-macos-amd64.tar.gz"
     sha256 "9f6b7371fe0f1e9d313e02ec1d34d15aa05b20caa34b6abf077655811a4c78ea"
