50886bfbf0c3506d8b8c8613db22c0a079ec3489
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-07 17:04:36 +0300

    Update Fin to version 0.4.0 with new URLs and SHA256

--- Formula/fin.rb
+++ Formula/fin.rb
@@ -1,25 +1,25 @@
 class Fin < Formula
   desc "a neon-electric TUI Jellyfin & Navidrome client - powered by symphonia, mpv, Chromecast & UPnP MediaRenderer"
   homepage "https://github.com/tsirysndr/fin"
-  url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-aarch64.tar.gz"
-  sha256 "7b755b4790cf4719f06b4bfe461f0c7f017e5464b94bf931b429fdcae27f3e86"
-  version "0.3.1"
+  url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-aarch64.tar.gz"
+  sha256 "2ed1836058dd79ab2fb0bd1a6b514772b1227a207a9d4437f28042af51e9888e"
+  version "0.4.0"
 
   depends_on "mpv"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-amd64.tar.gz"
-    sha256 "fa78474bddb92fa2e40e8e10ae25dfe38dc9b1a48abbc08ce1fad546c474e1dc"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-amd64.tar.gz"
+    sha256 "3cd9caf70daeba533ea8c06f50c5cfec1713c721466d73e0bbf2fb69f8f7409b"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-macos-aarch64.tar.gz"
-    sha256 "7b755b4790cf4719f06b4bfe461f0c7f017e5464b94bf931b429fdcae27f3e86"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-aarch64.tar.gz"
+    sha256 "2ed1836058dd79ab2fb0bd1a6b514772b1227a207a9d4437f28042af51e9888e"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.3.1/fin-v0.3.1-linux-amd64.tar.gz"
-    sha256 "0bf4f03d642932308193cff97036b2fa01006fc92d44d3d1e65f04c7048d848d"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-linux-amd64.tar.gz"
+    sha256 "d7ca69ddab0300669c71eee1c03c8a99036dc8f2f8ae2c5f72b0eb463495e8a6"
   end
 
   def install
