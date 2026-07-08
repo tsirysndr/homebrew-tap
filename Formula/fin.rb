636ef35208c4de843d9b1266af30690f3512d8f5
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-08 18:42:40 +0300

    Update Fin to version 0.5.0 with new URLs and checksums

--- Formula/fin.rb
+++ Formula/fin.rb
@@ -1,25 +1,25 @@
 class Fin < Formula
   desc "a neon-electric TUI Jellyfin & Navidrome client - powered by symphonia, mpv, Chromecast & UPnP MediaRenderer"
   homepage "https://github.com/tsirysndr/fin"
-  url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-aarch64.tar.gz"
-  sha256 "2ed1836058dd79ab2fb0bd1a6b514772b1227a207a9d4437f28042af51e9888e"
-  version "0.4.0"
+  url "https://github.com/tsirysndr/fin/releases/download/v0.5.0/fin-v0.5.0-macos-aarch64.tar.gz"
+  sha256 "d5f4af1e2ebbec5358533176645c79f51fcd461bf1f6f63f57e1dbaa9c5eb8d9"
+  version "0.5.0"
 
   depends_on "mpv"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-amd64.tar.gz"
-    sha256 "3cd9caf70daeba533ea8c06f50c5cfec1713c721466d73e0bbf2fb69f8f7409b"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.5.0/fin-v0.5.0-macos-amd64.tar.gz"
+    sha256 "ba952ac9ac5aee717f305897955404c7ff4bfeeaf1557f3b87b36f0efee6f1da"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-macos-aarch64.tar.gz"
-    sha256 "2ed1836058dd79ab2fb0bd1a6b514772b1227a207a9d4437f28042af51e9888e"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.5.0/fin-v0.5.0-macos-aarch64.tar.gz"
+    sha256 "d5f4af1e2ebbec5358533176645c79f51fcd461bf1f6f63f57e1dbaa9c5eb8d9"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/fin/releases/download/v0.4.0/fin-v0.4.0-linux-amd64.tar.gz"
-    sha256 "d7ca69ddab0300669c71eee1c03c8a99036dc8f2f8ae2c5f72b0eb463495e8a6"
+    url "https://github.com/tsirysndr/fin/releases/download/v0.5.0/fin-v0.5.0-linux-amd64.tar.gz"
+    sha256 "5dde49758193e8470bd31b3cc8148e7a76cbc505d55231ba9099724213c3abc6"
   end
 
   def install
