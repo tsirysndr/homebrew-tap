04262c87bad4a467fbf659d90c775af294be38dd
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-11 07:04:56 +0300

    Update Tunein to version 0.7.1

--- Formula/tunein.rb
+++ Formula/tunein.rb
@@ -1,23 +1,23 @@
 class Tunein < Formula
   desc "Browse and listen to thousands of radio stations across the globe right from your terminal"
   homepage "https://github.com/tsirysndr/tunein-cli"
-  url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_aarch64-apple-darwin.tar.gz"
-  sha256 "3ccfe74e4e0b3a2e3e6e0874d1864da2924ecd514a91f735e6132cc9d501f099"
-  version "0.7.0"
+  url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.1/tunein_v0.7.1_aarch64-apple-darwin.tar.gz"
+  sha256 "351ac04387c082dbcc3c25541f366c305cdef447a64ec5eda9292be487a857fe"
+  version "0.7.1"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_x86_64-apple-darwin.tar.gz"
-    sha256 "3a7ff48f75ff02f195cedc5c158a9956a0edee4125e632ab6e81699dd8c57660"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.1/tunein_v0.7.1_x86_64-apple-darwin.tar.gz"
+    sha256 "e8535e4927ac3b32c65e16a23967f67866c6ebae69cf247e7d00ce4d27e3c2e6"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_aarch64-apple-darwin.tar.gz"
-    sha256 "3ccfe74e4e0b3a2e3e6e0874d1864da2924ecd514a91f735e6132cc9d501f099"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.1/tunein_v0.7.1_aarch64-apple-darwin.tar.gz"
+    sha256 "351ac04387c082dbcc3c25541f366c305cdef447a64ec5eda9292be487a857fe"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "f1b89e00e99f9d576bd51df084c33732085478aa5726d88b173244e76bb63962"
+    url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.1/tunein_v0.7.1_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "93352d50c81ebf821d6b82ccc21882f23f951c333ba7f0b54c89a0a0a53121fa"
   end
 
   def install
