52a0a222da609655830388f91838d42a33bfd6d3
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-11 05:34:14 +0300

    Update SHA256 checksums for tunein formula

--- Formula/tunein.rb
+++ Formula/tunein.rb
@@ -2,22 +2,22 @@ class Tunein < Formula
   desc "Browse and listen to thousands of radio stations across the globe right from your terminal"
   homepage "https://github.com/tsirysndr/tunein-cli"
   url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_aarch64-apple-darwin.tar.gz"
-  sha256 "138d0294d9ea3b1731fbf4150f333d53f99ef3742aef7a6ddebb1c6e6aef231a"
+  sha256 "3ccfe74e4e0b3a2e3e6e0874d1864da2924ecd514a91f735e6132cc9d501f099"
   version "0.7.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_x86_64-apple-darwin.tar.gz"
-    sha256 "952accb3a4957891348e63a95db9d419537c68ecfb5061aa44d1236b7bc6a184"
+    sha256 "3a7ff48f75ff02f195cedc5c158a9956a0edee4125e632ab6e81699dd8c57660"
   end
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_aarch64-apple-darwin.tar.gz"
-    sha256 "138d0294d9ea3b1731fbf4150f333d53f99ef3742aef7a6ddebb1c6e6aef231a"
+    sha256 "3ccfe74e4e0b3a2e3e6e0874d1864da2924ecd514a91f735e6132cc9d501f099"
   end
 
   if OS.linux? && Hardware::CPU.intel?
     url "https://github.com/tsirysndr/tunein-cli/releases/download/v0.7.0/tunein_v0.7.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "4baa8dc10de561f975b3403aedf618c2f54d00cfc290e78a086c9891a54788a7"
+    sha256 "f1b89e00e99f9d576bd51df084c33732085478aa5726d88b173244e76bb63962"
   end
 
   def install
