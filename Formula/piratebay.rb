222c510426d70db2e83f3494030b773042f11d55
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-11 10:35:29 +0300

    Update piratebay formula to version 0.4.0

--- Formula/piratebay.rb
+++ Formula/piratebay.rb
@@ -1,22 +1,23 @@
 class Piratebay < Formula
   desc "A command-line tool written in Rust to search and download for torrents on piratebay"
   homepage "https://github.com/tsirysndr/piratebay"
-  url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-apple-darwin.tar.gz"
-  sha256 "9d8b9a61c71098169d6532591a7c67652903e930cca7817a107dac0ec592ec74"
+  url "https://github.com/tsirysndr/piratebay/releases/download/v0.4.0/piratebay_v0.4.0_aarch64-apple-darwin.tar.gz"
+  sha256 "3536a5ca9718bdff1093a9490126bdc5c320b99f7c93ae64945a92e4c996527d"
+  version "0.4.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-apple-darwin.tar.gz"
-    sha256 "9d8b9a61c71098169d6532591a7c67652903e930cca7817a107dac0ec592ec74"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.4.0/piratebay_v0.4.0_x86_64-apple-darwin.tar.gz"
+    sha256 "8d2bf2c3b4ce4b022472c5086d6bbe49dc5d0bc023727dcc6a51f71498811a7f"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_aarch64-apple-darwin.tar.gz"
-    sha256 "cd20c2d8af7c453831b94e42a0fbcdf83d900c1da32a2713da1fd71a45bc3c25"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.4.0/piratebay_v0.4.0_aarch64-apple-darwin.tar.gz"
+    sha256 "3536a5ca9718bdff1093a9490126bdc5c320b99f7c93ae64945a92e4c996527d"
   end
   
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/piratebay/releases/download/v0.3.0/piratebay_v0.3.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "12330a444bce1b4d23fded131de4939ac446887f5b72d548f93859fc2e1834df"
+    url "https://github.com/tsirysndr/piratebay/releases/download/v0.4.0/piratebay_v0.4.0_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "81a0dda1c001317ac1dc31376fff6b3e8b661825e278cbfa6cbaac70e92ba9bb"
   end
 
   def install
