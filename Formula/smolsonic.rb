3283db2838ccfbe169a9f9c2328636ccd2e08d18
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-16 21:09:40 +0300

    Update Smolsonic to version 0.10.0

--- Formula/smolsonic.rb
+++ Formula/smolsonic.rb
@@ -1,23 +1,23 @@
 class Smolsonic < Formula
   desc "A tiny Subsonic server in Rust"
   homepage "https://github.com/tsirysndr/smolsonic"
-  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.9.0/smolsonic-v0.9.0-macos-aarch64.tar.gz"
-  sha256 "de648b3d329b82a3d382bbbc719384a683b12daff47065ae6c5936bdf2d21292"
-  version "0.9.0"
+  url "https://github.com/tsirysndr/smolsonic/releases/download/v0.10.0/smolsonic-v0.10.0-macos-aarch64.tar.gz"
+  sha256 "4abb198919a34e4d58e789c33bee576215c642f30b5ff13430c08311dfec844d"
+  version "0.10.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.9.0/smolsonic-v0.9.0-macos-amd64.tar.gz"
-    sha256 "3a425d1955619676570177b572a7b84ebeb12343aa76f955cc976155c25d2fee"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.10.0/smolsonic-v0.10.0-macos-amd64.tar.gz"
+    sha256 "7c8eda0beba5ae027de76093d5ce1289b7714e4c98676c2646cb5e15c7b7c362"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.9.0/smolsonic-v0.9.0-macos-aarch64.tar.gz"
-    sha256 "de648b3d329b82a3d382bbbc719384a683b12daff47065ae6c5936bdf2d21292"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.10.0/smolsonic-v0.10.0-macos-aarch64.tar.gz"
+    sha256 "4abb198919a34e4d58e789c33bee576215c642f30b5ff13430c08311dfec844d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.9.0/smolsonic-v0.9.0-linux-amd64.tar.gz"
-    sha256 "cfd01ffd9fae3c875f020ebe265f78505d2ed9e667c53fc131d9d8b57a2d0fbd"
+    url "https://github.com/tsirysndr/smolsonic/releases/download/v0.10.0/smolsonic-v0.10.0-linux-amd64.tar.gz"
+    sha256 "54c98d92668b43ff28350ccac6a8ecca6e352fb3262165b1ae55d087a07fb10c"
   end
 
   def install
