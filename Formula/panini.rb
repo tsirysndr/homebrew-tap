590e2f43e35a91df26b65f9d3fd4b0391955636f
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-07-30 23:27:07 +0300

    Update Panini to version 0.2.0 with new URLs

--- Formula/panini.rb
+++ Formula/panini.rb
@@ -1,23 +1,23 @@
 class Panini < Formula
   desc "Press a Gleam (Erlang/BEAM) app into a single self-contained binary — a Burrito for Gleam."
   homepage "https://github.com/tsirysndr/panini"
-  url "https://github.com/tsirysndr/panini/releases/download/v0.1.0/panini-v0.1.0-aarch64-macos.tar.gz"
-  sha256 "84c01b3d9fb570f4ff6f908b89dccfe3f11b5214504d1f29f7d3a93f9aca5891"
-  version "0.1.0"
+  url "https://github.com/tsirysndr/panini/releases/download/v0.2.0/panini-v0.2.0-aarch64-macos.tar.gz"
+  sha256 "fcf0d44312b3900c2b1219a75a793fc73094cc0addfb13a4460f77e5a7737e90"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "60d5b1a8d639907ac594e9c73fe851454bc532bf5f2acdfd1c6da49564fe79a4"
-    sha256 "https://github.com/tsirysndr/panini/releases/download/v0.1.0/panini-v0.1.0-x86_64-macos.tar.gz"
+    url "7aaa14f09df3df907d3f577bb49c02a41f73cb3d75de58f67168d8bb5844f9c3"
+    sha256 "https://github.com/tsirysndr/panini/releases/download/v0.2.0/panini-v0.2.0-x86_64-macos.tar.gz"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/panini/releases/download/v0.1.0/panini-v0.1.0-aarch64-macos.tar.gz"
-    sha256 "84c01b3d9fb570f4ff6f908b89dccfe3f11b5214504d1f29f7d3a93f9aca5891"
+    url "https://github.com/tsirysndr/panini/releases/download/v0.2.0/panini-v0.2.0-aarch64-macos.tar.gz"
+    sha256 "fcf0d44312b3900c2b1219a75a793fc73094cc0addfb13a4460f77e5a7737e90"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/panini/releases/download/v0.1.0/panini-v0.1.0-x86_64-linux.tar.gz"
-    sha256 "ba14af9d9625f9f669d106eee7ed634bea2cc03287b4924d6ccaa241e35e86fd"
+    url "https://github.com/tsirysndr/panini/releases/download/v0.2.0/panini-v0.2.0-x86_64-linux.tar.gz"
+    sha256 "4aac17607bfe4611002ddfd3d274b64740b6494ced52ecfea7bc2be169247fbe"
   end
 
   def install
