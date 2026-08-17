22d9cda2790b496d1de16420575d99134790bf78
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-08-17 11:47:18 +0300

    Update cvisor.rb

--- Formula/cvisor.rb
+++ Formula/cvisor.rb
@@ -1,19 +1,19 @@
 class Cvisor < Formula
   desc "Embedded bash sandbox for agents, inspired by gVisor"
   homepage "https://github.com/tsirysndr/cvisor"
-  url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.1/cvisor-darwin-aarch64.tar.gz"
-  sha256 "59ea961fbccef0fe77477ed7db97d7c08a97c4af654bdaeb85e20ee616872ef8"
-  version "0.1.1"
+  url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.2/cvisor-darwin-aarch64.tar.gz"
+  sha256 "e431fc4d3ba1decb42f7e5f800495a0d9aaef5094d9a423b7623523e13135405"
+  version "0.1.2"
 
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.1/cvisor-darwin-aarch64.tar.gz"
-    sha256 "59ea961fbccef0fe77477ed7db97d7c08a97c4af654bdaeb85e20ee616872ef8"
+    url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.2/cvisor-darwin-aarch64.tar.gz"
+    sha256 "e431fc4d3ba1decb42f7e5f800495a0d9aaef5094d9a423b7623523e13135405"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.1/cvisor-linux-x86_64.tar.gz"
-    sha256 "8351df7af17a5563d79469b455924f22a810a59a6d4402c19e0659bcdf066b6a"
+    url "https://github.com/tsirysndr/cVisor/releases/download/v0.1.2/cvisor-linux-x86_64.tar.gz"
+    sha256 "19dff8e3524031a36e5be74768cd5d2bafaa873eb8dff41ba67ac634da367e0d"
   end
 
   def install
