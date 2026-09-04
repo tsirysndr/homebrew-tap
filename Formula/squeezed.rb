dba113a88dabcbfd797a54a0b4789c4bd66676a9
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-04 21:42:44 +0300

    Update squeezed.rb

--- Formula/squeezed.rb
+++ Formula/squeezed.rb
@@ -1,23 +1,23 @@
 class Squeezed < Formula
   desc "Serve a raw PCM audio stream to any Squeezelite / Squeezebox client over the SlimProto protocol."
   homepage "https://github.com/tsirysndr/squeezed"
-  url "https://github.com/tsirysndr/squeezed/releases/download/v0.1.0/squeezed-v0.1.0-darwin-arm64.tar.gz"
-  sha256 "c48bb01eba706f03f9bedc316f87cdd9c328a8e3a589526e2e587765d24ece69"
-  version "0.1.0"
+  url "https://github.com/tsirysndr/squeezed/releases/download/v0.2.0/squeezed-v0.2.0-darwin-arm64.tar.gz"
+  sha256 "293628e9ad0a0e6caa5df29a44abf3c2fc691bf77e2e8717be052a5aee4f2f7d"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/squeezed/releases/download/v0.1.0/squeezed-v0.1.0-darwin-amd64.tar.gz"
-    sha256 "3e6a420f1626ffb307e17566f35310825db5c8a8434d1c939f316a8b10a15e45"
+    url "https://github.com/tsirysndr/squeezed/releases/download/v0.2.0/squeezed-v0.2.0-darwin-amd64.tar.gz"
+    sha256 "d48374fea5468189a0fdc7961cded4a41ea442dbf5455f821c21b7afca209e7e"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/squeezed/releases/download/v0.1.0/squeezed-v0.1.0-darwin-arm64.tar.gz"
-    sha256 "c48bb01eba706f03f9bedc316f87cdd9c328a8e3a589526e2e587765d24ece69"
+    url "https://github.com/tsirysndr/squeezed/releases/download/v0.2.0/squeezed-v0.2.0-darwin-arm64.tar.gz"
+    sha256 "293628e9ad0a0e6caa5df29a44abf3c2fc691bf77e2e8717be052a5aee4f2f7d"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/squeezed/releases/download/v0.1.0/squeezed-v0.1.0-linux-amd64.tar.gz"
-    sha256 "e492052fb96300eddaf5ce7a6f1ea4c0142c23f15a83f5f73e831fa98db0e15c"
+    url "https://github.com/tsirysndr/squeezed/releases/download/v0.2.0/squeezed-v0.2.0-linux-amd64.tar.gz"
+    sha256 "900e0fd6fd53710ab18b0728991581bec3bafc7e16761fa20501e5d7a3ee609f"
   end
 
   def install
