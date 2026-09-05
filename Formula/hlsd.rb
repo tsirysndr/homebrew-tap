2e933a45a7abb0a90b4dac8879f9092f3d7e1958
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-05 07:54:24 +0300

    Update hlsd.rb

--- Formula/hlsd.rb
+++ Formula/hlsd.rb
@@ -1,23 +1,23 @@
 class Hlsd < Formula
   desc "Serve live HLS (and optional MPEG-DASH) from a raw PCM s16le audio stream"
   homepage "https://github.com/tsirysndr/hlsd"
-  url "https://github.com/tsirysndr/hlsd/releases/download/v0.1.0/hlsd-v0.1.0-darwin-arm64.tar.gz"
-  sha256 "e7912c89472b1e298edd525d02f4338e1ebe92ec6f32d09f9f553177df562695"
-  version "0.1.0"
+  url "https://github.com/tsirysndr/hlsd/releases/download/v0.2.0/hlsd-v0.2.0-darwin-arm64.tar.gz"
+  sha256 "5f4dfdece174549a24060fbc7a9fd3c2b7a49168a86b0ffa6e94a702fdb58613"
+  version "0.2.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/hlsd/releases/download/v0.1.0/hlsd-v0.1.0-darwin-amd64.tar.gz"
-    sha256 "dac3e4ed97c61bca9e395a1732650331790c8e66444b8dd0ccf5982e8765862f"
+    url "https://github.com/tsirysndr/hlsd/releases/download/v0.2.0/hlsd-v0.2.0-darwin-amd64.tar.gz"
+    sha256 "4c67bee1d2ff3e224365cf6c3d1045c1c87182756a9ae19eeeb3421de88e1043"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/hlsd/releases/download/v0.1.0/hlsd-v0.1.0-darwin-arm64.tar.gz"
-    sha256 "e7912c89472b1e298edd525d02f4338e1ebe92ec6f32d09f9f553177df562695"
+    url "https://github.com/tsirysndr/hlsd/releases/download/v0.2.0/hlsd-v0.2.0-darwin-arm64.tar.gz"
+    sha256 "5f4dfdece174549a24060fbc7a9fd3c2b7a49168a86b0ffa6e94a702fdb58613"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/hlsd/releases/download/v0.1.0/hlsd-v0.1.0-linux-amd64.tar.gz"
-    sha256 "9bcf50dbf9f40091841d942b82fe18983e8d30e56763aa7658951f435ca7b3ac"
+    url "https://github.com/tsirysndr/hlsd/releases/download/v0.2.0/hlsd-v0.2.0-linux-amd64.tar.gz"
+    sha256 "4e270a1dfe18454f05fc7b2eae580d93e9b389270a6afaa25471fcf4372d715c"
   end
 
   def install
