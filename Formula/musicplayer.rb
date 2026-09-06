c93ab3ea27ae2ff049de72aa117bd2d752b73552
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-07 00:14:31 +0300

    Update music-player to version 0.2.1

--- Formula/musicplayer.rb
+++ Formula/musicplayer.rb
@@ -1,22 +1,22 @@
 class Musicplayer < Formula
   desc "An extensible music server written in Rust"
   homepage "https://github.com/tsirysndr/music-player"
-  url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0/music-player_v0.2.0_aarch64-apple-darwin.tar.gz"
-  sha256 "d399479754eaae6808afbcbbc37d536a7ac619441c97090b78bf0dd82486342b"
+  url "https://github.com/tsirysndr/music-player/releases/download/v0.2.1/music-player_v0.2.1_aarch64-apple-darwin.tar.gz"
+  sha256 "4d2cddc813a63845e30dfe9ff8facef75215a82e88f43962d4b1b84c2021e882"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0/music-player_v0.2.0_x86_64-apple-darwin.tar.gz"
-    sha256 "cef585fc1d3679a6c78dd9802527dd552e8e32cbf6a20f0e896cd102aacac361"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.1/music-player_v0.2.1_x86_64-apple-darwin.tar.gz"
+    sha256 "03ec32fe293bafbd5c92a447b9a56cdbee70ac667edffc9f8e2bc6af51fa57dc"
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0/music-player_v0.2.0_aarch64-apple-darwin.tar.gz"
-    sha256 "d399479754eaae6808afbcbbc37d536a7ac619441c97090b78bf0dd82486342b"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.1/music-player_v0.2.1_aarch64-apple-darwin.tar.gz"
+    sha256 "4d2cddc813a63845e30dfe9ff8facef75215a82e88f43962d4b1b84c2021e882"
   end
 
   if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.0/music-player_v0.2.0_x86_64-unknown-linux-gnu.tar.gz"
-    sha256 "5449b3f6fe773f8caaa50ea3bad6eaec8b5a2fbde85fd2893d01364ec062ec1e"
+    url "https://github.com/tsirysndr/music-player/releases/download/v0.2.1/music-player_v0.2.1_x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "ab9647b1cb04aea9aed830bf899304bd8289f2d0c7a776662392afca4c7a26bd"
   end
 
   def install
