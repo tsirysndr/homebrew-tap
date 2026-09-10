308430fa195768cbfff998ec54cf6c6eafde0bba
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-10 21:05:04 +0300

    Add rocksky cask for Rocksky Desktop 0.1.0

---
+++ Casks/rocksky.rb
@@ -0,0 +1,30 @@
+cask "rocksky" do
+  arch arm: "aarch64", intel: "x64"
+
+  version "0.1.0"
+  sha256 arm:   "96e154ce63d9f2b45f914f9593a4be296b4783fd3fba43cf38242131db64bc1b",
+         intel: "38fced45c19409725e3fc97dd347c12c1369bb41827c7dd85364995603fc0461"
+
+  url "https://github.com/tsirysndr/rocksky/releases/download/desktop-v#{version}/Rocksky_#{version}_#{arch}.dmg"
+  name "Rocksky"
+  desc "Decentralized music tracking and discovery platform built on AT Protocol"
+  homepage "https://github.com/tsirysndr/rocksky"
+
+  livecheck do
+    url :url
+    regex(/^desktop[._-]v?(\d+(?:\.\d+)+)$/i)
+    strategy :git
+  end
+
+  depends_on :macos
+
+  app "Rocksky.app"
+
+  zap trash: [
+    "~/Library/Application Support/app.rocksky.desktop",
+    "~/Library/Caches/app.rocksky.desktop",
+    "~/Library/Preferences/app.rocksky.desktop.plist",
+    "~/Library/Saved Application State/app.rocksky.desktop.savedState",
+    "~/Library/WebKit/app.rocksky.desktop",
+  ]
+end
