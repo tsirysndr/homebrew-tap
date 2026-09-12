8bb9672730a299ad8dce75c78780e2382f0b8303
Tsiry Sandratraina <tsiry.sndr@rocksky.app>
2026-09-12 23:04:48 +0300

    casks: refresh sha256 for re-uploaded rockbox and musicplayer assets

--- Casks/musicplayer.rb
+++ Casks/musicplayer.rb
@@ -2,8 +2,8 @@ cask "musicplayer" do
   arch arm: "aarch64", intel: "x86_64"
 
   version "0.3.0"
-  sha256 arm:   "fde1d7625cedba9b66cac5684fabfe51b5005755d4b6324ee5399eaed7236da1",
-         intel: "3a3b312d90ad39d4509d2fed16cfbc0f86f402f3f498295f4be787150e8d63a2"
+  sha256 arm:   "4d4a29595050ccdb58d7d967108faa04b271837c5dc95c8ca943b9fe4f8bf5c7",
+         intel: "85cfd614d440f6b6011b7855bf5de735cbc05c7e00f956c21d70f2db96ac9453"
 
   url "https://github.com/tsirysndr/music-player/releases/download/v#{version}/music-player-desktop_v#{version}_#{arch}-apple-darwin.tar.gz"
   name "musicplayer"
